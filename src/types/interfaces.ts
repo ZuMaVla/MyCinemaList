b9a9caaeb0fcfd04c2c1d29c486ba55a4a4d6db1
ZuMaVla <marinazubialevich@gmail.com>
2025-06-23 21:22:32 +0100

    Added routing and live data from API

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -15,6 +15,7 @@ export interface BaseMovieProps {
     revenue: number;
     vote_count: number;
     favourite?: boolean;
+    genre_ids?: number[];
 }
 
 export interface BaseMovieListProps { 
@@ -47,4 +48,5 @@ export interface MoviePageProps {
   images: MovieImage[];
 }
 
+export type FilterOption = "title" | "genre";
 
